cbe47914e390104070bd396933eb6c73d13e51b3
b12s307 <juanandrescuervoaguirre8@gmail.com>
2023-02-10 17:35:08 -0500

    first commit

--- algo.py
+++ algo.py
@@ -1,14 +1,14 @@
+import random 
 def crearMatriz(n,matriz = []):   #n**2
-  for i in range(n):
+  for i in range(n): 
     matriz.append([]) 
-    for j in range(n):
-      matriz[i].append(i*j)
+    for j in range(n): 
+      matriz[i].append(random.randint(1,99))
   return Mostrar(matriz)
 def Mostrar(matriz):  #n
   [print(x) for x in matriz]
   return sumaTotal(matriz)
 def sumaTotal(lista,suma=0): #n  + n
-  for i in lista:
-    suma+=sum(i)
+  for i in lista: suma=suma+sum(i)
   return suma
-crearMatriz(3)
+crearMatriz(4)
