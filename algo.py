94f8e3818f4aa06f752a3b27738e70a305a91fbc
b12s307 <juanandrescuervoaguirre8@gmail.com>
2023-02-10 17:20:06 -0500

    first commit

---
+++ algo.py
@@ -0,0 +1,14 @@
+def crearMatriz(n,matriz = []):   #n**2
+  for i in range(n):
+    matriz.append([]) 
+    for j in range(n):
+      matriz[i].append(i*j)
+  return Mostrar(matriz)
+def Mostrar(matriz):  #n
+  [print(x) for x in matriz]
+  return sumaTotal(matriz)
+def sumaTotal(lista,suma=0): #n  + n
+  for i in lista:
+    suma+=sum(i)
+  return suma
+crearMatriz(3)
