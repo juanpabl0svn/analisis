d4cb2c27e351f47b8bfbb5d311f0690f424e5e1c
JuanPabloSanVille <87534135+juanpabl0svn@users.noreply.github.com>
2023-02-11 15:40:03 -0500

    actividad_listas
    ahora tiene 12 lineas

--- algo.py
+++ algo.py
@@ -1,14 +1,12 @@
 import random 
-def crearMatriz(n,matriz = []):   #n**2
+def crearMatriz(n,matriz = []):#O(n**2)
   for i in range(n): 
-    matriz.append([]) 
-    for j in range(n): 
-      matriz[i].append(random.randint(1,99))
-  return Mostrar(matriz)
-def Mostrar(matriz):  #n
+    matriz.append([])
+    [matriz[i].append(random.randint(1,99)) for j in range(n)]
+  return sumaTotal(matriz),Mostrar(matriz)
+def Mostrar(matriz):  #O(n)
   [print(x) for x in matriz]
-  return sumaTotal(matriz)
-def sumaTotal(lista,suma=0): #n  + n
-  for i in lista: suma=suma+sum(i)
+def sumaTotal(lista,suma=0): #O(n) 
+  for i in lista: suma+=+sum(i)
   return suma
-crearMatriz(4)
+crearMatriz(4)[0]
