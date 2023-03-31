2b07f67dc0dd27754ab36e1d6dca771e86ac5938
JuanPabloSanVille <87534135+juanpabl0svn@users.noreply.github.com>
2023-03-31 16:31:43 -0500

    Create punto2_JuanPabloSanchez.py
    punto 2 del examen de análisis y diseño de algoritmos

---
+++ quiz/punto2_JuanPabloSanchez.py
@@ -0,0 +1,21 @@
+def potencia_recursivo(numero,potencia):    #O(n^n)
+  if potencia == 0:
+    return 1
+  if potencia == 1:
+    return numero
+  if potencia < 0:
+    return 1/numero * potencia_recursivo(numero,potencia+1)
+  return numero * potencia_recursivo(numero,potencia-1)
+
+def potencia_no_recursivo(numero,potencia):      # =  O(n)
+  resultado = 1                             #O(1)
+  if potencia < 0:                          #O(1)
+    for i in range(0,-potencia):            #O(n)
+      resultado*=1/numero                   #O(n)
+  else:
+    for i in range(0,potencia):             #O(n)
+      resultado*=numero                     #O(n)
+  return resultado
+
+potencia_no_recursivo(3,3)
+potencia_recursivo(3,3)
